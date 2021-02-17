7fb3b0933f2647f712361027fc749fba6d232341
osman <osman.anr@gmail.com>
2021-02-17 16:29:46 +0300

    17/02/2021

---
+++ OOP2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Osman";
+            musteri1.Soyadi = "ANAR";
+            musteri1.TcNo = "123456789";
+
+            //Kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "67895";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "122345678";
+
+
+            //Gerçek Müşteri - Tüzel Müşteri
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+        }
+    }
+}
---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager,ILoggerService loggerService)//--->Bunu yaparsak konut taşıt ihtiyaç hangisini seçersek seçelim hesaplar
+        {
+            //Başvuran bilgilerini değerlendirme
+            //
+            //KonutKrediManager konutKrediManager = new KonutKrediManager();
+            //konutKrediManager.Hesapla(); böyle yaparsak tüm başvuruları konut kredisine  bağımlı hale getiririz Yanlış
+            krediManager.Hesapla();
+            loggerService.Log();
+
+        }
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
